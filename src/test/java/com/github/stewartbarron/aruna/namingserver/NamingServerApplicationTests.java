d5dbd1897caac42b62af34d9dd5b0f9feda0005f
Stewart Barron <stewartbarron@Stewarts-iMac.local>
2021-12-13 22:42:34 +0000

    intial

---
+++ src/test/java/com/github/stewartbarron/aruna/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.stewartbarron.aruna.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
